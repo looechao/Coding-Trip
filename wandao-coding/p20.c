df9e8202b14c430074cb2ed98eaebf60b20bb431
looechao <lu.chao.personal@foxmail.com>
2024-04-09 22:15:47 +0800

    update

--- wandao-coding/p20.c
+++ wandao-coding/p20.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+
+    return 0;
+}
+
+void itoa(int,char)
+{
+
+}
---
+++ wandao-coding/p25.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main()
+{
+    char s1[100]={"My name is Li jilin."};
+    char s2[100]={"Mr. Zhang Haoling is very happy."};
+    printf("%s \n",s1);
+    printf("%s",s2);
+    return 0;
+}
