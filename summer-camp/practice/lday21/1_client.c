12b4d5694132380d14a218c34d848de7cd2b9a64
looechao <lu.chao.personal@foxmail.com>
2024-06-17 09:11:26 +0800

    update

--- summer-camp/practice/lday21/1_client.c
+++ summer-camp/practice/lday21/1_client.c
@@ -2,20 +2,22 @@
 
 int main(int argc, char* argv[])
 {
-    // ./1_client 192.168.118.128 1234
+    // ./1_client 192.168.68.128 1234
+    //socket 是两端通信的端点
     ARGS_CHECK(argc, 3);
-    int sockfd = socket(AF_INET,SOCK_STREAM,0);
-    struct sockaddr_in addr; //服务端地址
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(atoi(argv[2]));
     addr.sin_addr.s_addr = inet_addr(argv[1]);
     int ret = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     ERROR_CHECK(ret, -1, "connect");
-    //:sleep(5);
+    sleep(5);
     printf("sleep over\n");
     ssize_t sret = send(sockfd, "hello", 5, 0);
     printf("sret = %ld\n", sret);
     sret = send(sockfd, "world", 5, 0);
+    //sret = write(sockfd, "world", 5);
     printf("sret = %ld\n", sret);
     return 0;
 }
--- summer-camp/practice/lday21/1_server.c
+++ summer-camp/practice/lday21/1_server.c
@@ -2,24 +2,27 @@
 
 int main(int argc, char* argv[])
 {
+    // ./1_server.c 192.168.68.128 1234
     ARGS_CHECK(argc, 3);
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr;   //填写服务端地址
+    struct sockaddr_in addr; //服务端地址的结构体初始化
     addr.sin_family = AF_INET;
+    //向ip结构体中传入ip地址和端口号参数
     addr.sin_port = htons(atoi(argv[2]));
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
     int ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     ERROR_CHECK(ret, -1, "bind");
-    listen(sockfd,10);
+    listen(sockfd, 10);   //DDOS攻击的点
     struct sockaddr_in clientAddr;
     socklen_t clientAddrSize = sizeof(clientAddr); //该变量必须初始化
-    int netfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrSize);
+    int netfd =  accept(sockfd,(struct sockaddr *)&clientAddr, &clientAddrSize);
     printf("netfd = %d\n", netfd);
     printf("client ip = %s, port = %d\n",
            inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-    char buf[4096] = {0};
-    sleep(1);
+    char buf[4096];
+    //sleep(5);
     ssize_t sret = recv(netfd, buf, sizeof(buf), 0);
-    printf("sert = %ld, buf = %s\n", sret, buf);
+    printf("sret = %ld, buf = %s\n", sret, buf);
     return 0;
 }
 
