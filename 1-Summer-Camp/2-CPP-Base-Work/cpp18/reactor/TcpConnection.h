9e8d357c400a3a92c954219e5355fd895d5a869e
looechao <lu.chao.personal@foxmail.com>
2024-07-30 22:32:42 +0800

    update reactor

---
+++ 1-Summer-Camp/2-CPP-Base-Work/cpp18/reactor/TcpConnection.h
@@ -0,0 +1,33 @@
+#ifndef __TCPCONNECTION_H__
+#define __TCPCONNECTION_H__
+
+#include "Socket.h"
+#include "SocketIO.h"
+#include "InetAddress.h"
+
+class TcpConnection
+{
+public:
+    explicit TcpConnection(int fd);
+    ~TcpConnection();
+    void send(const string &msg);
+    string receive();
+
+    //为了方便调试的函数
+    string toString();
+
+private:
+    //获取本端地址与对端地址
+    InetAddress getLocalAddr();
+    InetAddress getPeerAddr();
+
+private:
+    SocketIO _sockIO;
+
+    //为了调试而加入的三个数据成员
+    Socket _sock;
+    InetAddress _localAddr;
+    InetAddress _peerAddr;
+};
+
+#endif
